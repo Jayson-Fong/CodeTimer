1013b14e4a8838aa96d36ebfbd35c98e0271c748
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-09-08 17:34:04 -0400

    Create ctTest.php

---
+++ ctTest.php
@@ -0,0 +1,29 @@
+<?php
+function executedCode() {
+    // Place Code to Time Here
+}
+require __DIR__ . '/src/CodeTimer/CodeTimer.php';
+function getTrialCount() {
+    echo PHP_EOL . "\e[1;32mTrial Count:\e[0m ";
+    $handle = fopen ("php://stdin","r");
+    $line = fgets ($handle);
+    $trials = trim($line);
+    if (!ctype_digit($trials) || $trials < 1) {
+        echo PHP_EOL . "\e[1;91mTrial count must be a whole number above or equal to one!\e[0m";
+        $trials = getTrialCount();
+    }
+    return $trials;
+}
+$trials = getTrialCount();
+echo PHP_EOL . "\e[1;32mRunning \e[1;31m" . $trials . " \e[1;32mtrials in \e[1;31m3\e[1;32m seconds!\e[0m";
+sleep(3);
+$codeTimer = new CodeTimer();
+$i = 0;
+while ($i++ < $trials) {
+    $codeTimer->startTime();
+    executedCode();
+    $codeTimer->stopTime();
+    echo (PHP_EOL . "\e[1;33m" . ($trials - $i) . " \e[1;32mTrials Remaining - \e[1;32m" . (float)($i / $trials)*100 . "%\e[1;32m Complete\e[0m" . PHP_EOL);
+}
+echo "\e[1;31m" . $trials . " \e[1;32mTrials Successful\e[0m";
+echo $codeTimer->getTime(true);
