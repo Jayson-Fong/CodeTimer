e7699aca6dd909338d39a409b3450748d47bee29
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-09-09 08:55:38 -0400

    Added Memory Usage

--- ctTest.php
+++ ctTest.php
@@ -3,6 +3,7 @@ function executedCode() {
     // Place Code to Time Here
 }
 require __DIR__ . '/src/CodeTimer/CodeTimer.php';
+require __DIR__ . '/src/CodeTimer/Resources.php';
 function getTrialCount() {
     echo PHP_EOL . "\e[1;32mTrial Count:\e[0m ";
     $handle = fopen ("php://stdin","r");
@@ -17,13 +18,15 @@ function getTrialCount() {
 $trials = getTrialCount();
 echo PHP_EOL . "\e[1;32mRunning \e[1;31m" . $trials . " \e[1;32mtrials in \e[1;31m3\e[1;32m seconds!\e[0m";
 sleep(3);
-$codeTimer = new CodeTimer();
+$codeTimer = new CodeTimer_CodeTimer();
+$codeTimerRss = new CodeTimer_Resources(true);
 $i = 0;
+$codeTimerRss->setStartRss();
 while ($i++ < $trials) {
     $codeTimer->startTime();
     executedCode();
     $codeTimer->stopTime();
-    echo (PHP_EOL . "\e[1;33m" . ($trials - $i) . " \e[1;32mTrials Remaining - \e[1;32m" . (float)($i / $trials)*100 . "%\e[1;32m Complete\e[0m" . PHP_EOL);
+    echo (PHP_EOL . "\e[1;33m" . ($trials - $i) . " \e[1;32mTrials Remaining - \e[1;32m" . (float)($i / $trials)*100 . "%\e[1;32m Complete - Using \e[1;31m" . $codeTimerRss->getCurrentRss()->memory . "\e[1;32mMBs of Memory\e[0m" . PHP_EOL);
 }
 echo "\e[1;31m" . $trials . " \e[1;32mTrials Successful\e[0m";
 echo $codeTimer->getTime(true);
